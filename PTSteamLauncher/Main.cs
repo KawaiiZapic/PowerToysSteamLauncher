9f60fc266109e92ddb2eabfd2a0c6220919401a1
KawaiiZapic <kawaiizapic@zapic.moe>
2024-08-26 13:16:38 +0800

    feat: support display localization game name & hd icon

--- PTSteamLauncher/Main.cs
+++ PTSteamLauncher/Main.cs
@@ -6,7 +6,10 @@ using Wox.Infrastructure;
 using Wox.Plugin;
 
 namespace Community.PowerToys.Run.Plugin.SteamLauncher {
-    public partial class Main: IPlugin, IReloadable {
+    public partial class Main: IPlugin, IReloadable, IContextMenu {
+
+        [GeneratedRegex("\"path\"\\s*\"([^\"]*)\"")]
+        private static partial Regex LibraryPathMather();
         public string Name => "Steam Launcher";
         public static string PluginID => "9e13e2aa-da92-4094-84f8-6f2e2d3e90db";
 
@@ -16,7 +19,7 @@ namespace Community.PowerToys.Run.Plugin.SteamLauncher {
 
         private string _InitialzedFailedReason = "";
 
-        private string? SteamPath;
+        private string SteamPath = "";
 
         private List<SteamGame> steamGames = new List<SteamGame>();
 
@@ -40,18 +43,16 @@ namespace Community.PowerToys.Run.Plugin.SteamLauncher {
                     }
                     ];
             }
-            if (SteamPath == null) {
-                throw new Exception("Can't detected Steam installation path.");
-            }
             List<Result> results = [];
             foreach (SteamGame game in steamGames) {
-                if (StringMatcher.FuzzySearch(query.Search, game.name).Success) {
+                if (StringMatcher.FuzzySearch(query.Search, game.name).Success || (game.localizationName != null && StringMatcher.FuzzySearch(query.Search, game.localizationName).Success)) {
                     results.Add(new Result {
-                        Title = game.name,
+                        Title = game.localizationName ?? game.name,
                         SubTitle = "Steam Game",
-                        IcoPath = Path.Combine(SteamPath, "appcache", "librarycache", game.id + "_icon.jpg"),
+                        IcoPath = game.icon,
+                        ContextData = game.id,
                         Action = (e) => {
-                            Process.Start(Path.Combine(SteamPath, "steam.exe"), "steam://rungameid/" + game.id);
+                            Process.Start(Path.Combine(SteamPath, "steam.exe"), "steam://launch/" + game.id);
                             return true;
                         }
                     });
@@ -81,7 +82,7 @@ namespace Community.PowerToys.Run.Plugin.SteamLauncher {
             foreach (string line in SteamLibrariesData) {
                 var m = LibraryPathMather().Match(line);
                 if (m.Success) {
-                    SteamLibraries.Add(new SteamLibrary(Uri.UnescapeDataString(m.Groups[1].Value)));
+                    SteamLibraries.Add(new SteamLibrary(SteamPath, Uri.UnescapeDataString(m.Groups[1].Value)));
                 }
             }
 
@@ -93,12 +94,18 @@ namespace Community.PowerToys.Run.Plugin.SteamLauncher {
         struct SteamGame {
             public string id;
             public string name;
+            public string icon;
             public string? localizationName;
         }
 
-        partial class SteamLibrary(string _Path) {
-            private readonly DirectoryInfo _library = new(Path.Combine(_Path, "steamapps"));
+        partial class SteamLibrary(string SteamPath, string _Path) {
 
+            [GeneratedRegex("\"appid\"\\s*\"(\\d*)\"")]
+            private static partial Regex GameIdMatcher();
+            [GeneratedRegex("\"name\"\\s*\"([^\"]*)\"")]
+            private static partial Regex GameNameMatcher();
+
+            private readonly DirectoryInfo _library = new(Path.Combine(_Path, "steamapps"));
 
             private static readonly List<string> InternalBlockList = [
                 "228980" // Steamworks Shared
@@ -120,23 +127,46 @@ namespace Community.PowerToys.Run.Plugin.SteamLauncher {
                         continue;
                     }
                     var localizationName = Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Steam App " + id, "DisplayName", null)?.ToString();
+                    var icon = Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Steam App " + id, "DisplayIcon", null)?.ToString();
+                    if (!File.Exists(icon)) {
+                        icon = Path.Combine(SteamPath, "appcache", "librarycache", id + "_icon.jpg");
+                    }
                     FoundGames.Add(new SteamGame {
                         id = id,
                         name = name,
+                        icon = icon,
                         localizationName = localizationName
                     });
 
                 }
                 return [.. FoundGames];
             }
-
-            [GeneratedRegex("\"appid\"\\s*\"(\\d*)\"")]
-            private static partial Regex GameIdMatcher();
-            [GeneratedRegex("\"name\"\\s*\"([^\"]*)\"")]
-            private static partial Regex GameNameMatcher();
         }
 
-        [GeneratedRegex("\"path\"\\s*\"([^\"]*)\"")]
-        private static partial Regex LibraryPathMather();
+        public List<ContextMenuResult> LoadContextMenus(Result selectedResult) {
+            if (selectedResult.ContextData is not string id) {
+                return [];
+            }
+            return [
+                new ContextMenuResult {
+                    Glyph = "\xE768",
+                    FontFamily = "Segoe Fluent Icons,Segoe MDL2 Assets",
+                    Title = "Play",
+                    Action = (e) => {
+                        Process.Start(Path.Combine(SteamPath, "steam.exe"), "steam://launch/" + id);
+                        return true;
+                    }
+                },
+                new ContextMenuResult {
+                    Glyph = "\xE713",
+                    FontFamily = "Segoe Fluent Icons,Segoe MDL2 Assets",
+                    Title = "Open Game Config",
+                    Action = (e) => {
+                        Process.Start(Path.Combine(SteamPath, "steam.exe"), "steam://gameproperties/" + id);
+                        return true;
+                    }
+                },
+            ];
+        }
     }
 }
